3d71fca0ac9cd5d78aad971d0632ee38dc8e0206
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-10 13:56:39 +0100

    feat: jwt protection

--- src/main/java/com/blopa/demo/security/SecurityConfig.java
+++ src/main/java/com/blopa/demo/security/SecurityConfig.java
@@ -1,18 +1,41 @@
 package com.blopa.demo.security;
 
+import com.blopa.demo.security.filter.JwtFIlterRequest;
 import com.blopa.demo.services.DemoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DemoUserDetailsService demoUserDetailsService;
 
+    @Autowired
+    private JwtFIlterRequest jwtFIlterRequest;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(demoUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFIlterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
